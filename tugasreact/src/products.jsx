c3c1ff28758ee1483a67a4e439cd1a8d22ecc0cf
geofanno <geofanno710@gmail.com>
2023-11-10 08:20:01 +0800

    tugas

---
+++ tugasreact/src/products.jsx
@@ -0,0 +1,59 @@
+import product1 from "./assets/products/1.jpg";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.png";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.png";
+import product7 from "./assets/products/7.png";
+import product8 from "./assets/products/8.png";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "IPhone 15 Pro",
+    price: 1.649,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Macbook Pro 16 (M3)",
+    price: 4.699,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Ipad Air",
+    price: 887,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Apple Watch Ultra 2",
+    price: 1.199,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: " AirPods Pro",
+    price: 249,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "iMac",
+    price: 1.299,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Mac Studio",
+    price: 4.999,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Apple TV 4K",
+    price: 149,
+    productImage: product8,
+  },
+];
